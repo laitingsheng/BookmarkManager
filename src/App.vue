1b067e2c4beb5bf9f0cc969f052cf40215a5ef9b
Tinson Lai <laitingsheng@hotmail.com>
2025-07-19 21:15:58 +0800

    Add Chrome bookmarks listeners
    Signed-off-by: Tinson Lai <laitingsheng@hotmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import BookmarkTree from "./components/BookmarkTree.vue";
-import { use_bookmarks_store } from "./stores/bookmarks";
+import { use_bookmarks_store, type Bookmark } from "./stores/bookmarks";
 import { use_preferences_store } from "./stores/preferences";
 
 const bookmarks_store = use_bookmarks_store();
@@ -33,6 +33,129 @@ function switch_folderonly() {
 		},
 	);
 }
+
+chrome.bookmarks.onChanged.addListener((id, info) => {
+	if (chrome.runtime.lastError !== undefined) {
+		console.error(`chrome.bookmarks.onChanged: ${chrome.runtime.lastError.message}`);
+		return;
+	}
+	const bookmark = bookmarks_store.bookmarks[id];
+	if (bookmark === undefined) {
+		console.error(`Detected bookmark change for unknown id ${id}.`);
+		return;
+	}
+	bookmark.title = info.title;
+	if (info.url !== undefined) {
+		if (bookmark.folder) {
+			console.error(`chrome.bookmarks.onChanged: URL is assigned to folder #${id}.`);
+			return;
+		}
+		bookmark.url = info.url;
+	}
+});
+
+chrome.bookmarks.onChildrenReordered.addListener((id, info) => {
+	if (chrome.runtime.lastError !== undefined) {
+		console.error(`chrome.bookmarks.onChildrenReordered: ${chrome.runtime.lastError.message}`);
+		return;
+	}
+	const bookmark = bookmarks_store.bookmarks[id];
+	if (bookmark === undefined) {
+		console.error(`chrome.bookmarks.onChildrenReordered: Detected children reordering for unknown id ${id}.`);
+		return;
+	}
+	if (!bookmark.folder) {
+		console.error(`chrome.bookmarks.onChildrenReordered: Bookmark #${id} is not a folder.`);
+		return;
+	}
+	const children = new Array<Bookmark>();
+	for (const cid of info.childIds) {
+		const child = bookmarks_store.bookmarks[cid];
+		if (child === undefined) {
+			console.error(`chrome.bookmarks.onChildrenReordered: Bookmark #${id} has an unknown child id ${cid}.`);
+			continue;
+		}
+		children.push(child);
+	}
+	bookmark.children = children;
+});
+
+chrome.bookmarks.onCreated.addListener((id, node) => {
+	if (chrome.runtime.lastError !== undefined) {
+		console.error(`chrome.bookmarks.onCreated: ${chrome.runtime.lastError.message}`);
+		return;
+	}
+	if (node.parentId === undefined) {
+		console.error(`chrome.bookmarks.onCreated: Unexpected bookmark #${id} has no valid parent.`);
+		return;
+	}
+	const parent = bookmarks_store.bookmarks[node.parentId];
+	if (parent === undefined) {
+		console.error(`chrome.bookmarks.onCreated: Parent #${node.parentId} of bookmark #${id} is not found.`);
+		return;
+	}
+	if (!parent.folder) {
+		console.error(`chrome.bookmarks.onCreated: Parent #${node.parentId} of bookmark #${id} is not a folder.`);
+		return;
+	}
+	parent.children.push(bookmarks_store.traverse(node));
+});
+
+chrome.bookmarks.onMoved.addListener((id, info) => {
+	if (chrome.runtime.lastError !== undefined) {
+		console.error(`chrome.bookmarks.onMoved: ${chrome.runtime.lastError.message}`);
+		return;
+	}
+	const source = bookmarks_store.bookmarks[info.oldParentId];
+	if (source === undefined) {
+		console.error(`chrome.bookmarks.onMoved: Detected move of bookmark #${id} from unknown folder #${info.oldParentId}.`);
+		return;
+	}
+	if (!source.folder) {
+		console.error(`chrome.bookmarks.onMoved: Source #${info.oldParentId} of moved bookmark #${id} is not a folder.`);
+		return;
+	}
+	const target = bookmarks_store.bookmarks[info.parentId];
+	if (target === undefined) {
+		console.error(`chrome.bookmarks.onMoved: Detected move of bookmark #${id} to unknown folder #${info.parentId}.`);
+		return;
+	}
+	if (!target.folder) {
+		console.error(`chrome.bookmarks.onMoved: Target #${info.parentId} of moved bookmark #${id} is not a folder.`);
+		return;
+	}
+	if (info.oldIndex < source.children.length && source.children[info.oldIndex]?.id === id && info.index <= target.children.length) {
+		const bookmark = bookmarks_store.bookmarks[id];
+		if (bookmark === undefined) {
+			console.error(`chrome.bookmarks.onMoved: Detected move of unknown bookmark #${id}.`);
+			return;
+		}
+		source.children.splice(info.oldIndex, 1);
+		target.children.splice(info.index, 0, bookmark);
+	}
+});
+
+chrome.bookmarks.onRemoved.addListener((id, info) => {
+	if (chrome.runtime.lastError !== undefined) {
+		console.error(`chrome.bookmarks.onRemoved: ${chrome.runtime.lastError.message}`);
+		return;
+	}
+	const parent = bookmarks_store.bookmarks[info.parentId];
+	if (parent === undefined) {
+		console.error(`chrome.bookmarks.onRemoved: Parent #${info.parentId} of removed bookmark #${id} is not found.`);
+		return;
+	}
+	if (!parent.folder) {
+		console.error(`chrome.bookmarks.onRemoved: Parent #${info.parentId} of removed bookmark #${id} is not a folder.`);
+		return;
+	}
+	if (info.index < parent.children.length && parent.children[info.index]?.id === id) {
+		parent.children.splice(info.index, 1);
+		delete bookmarks_store.bookmarks[id];
+		return;
+	}
+	console.error(`chrome.bookmarks.onRemoved: Bookmark #${id} is not found in parent #${info.parentId} at index ${info.index}.`);
+});
 
 chrome.bookmarks.getTree((nodes) => {
 	if (chrome.runtime.lastError !== undefined) {
